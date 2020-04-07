59dfb874cfda87f010e32ba2696cae8d023e8d0b
Steph W <wilhelm.steph@gmail.com>
2020-04-07 22:45:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,3 @@
-def hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
-
 def hello_t(array)
   i = 0
 
@@ -13,4 +7,10 @@ def hello_t(array)
   end
 
   array
+end
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
